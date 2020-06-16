46224b2916d9ab9f7855f37367b16d690ad66232
mateus rodrigues <mateus037@gmail.com>
2020-06-16 15:12:22 -0300

    separando components com o cssgrid, adicionando a api do youtube

---
+++ src/components/SearchBar.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import {Segment, Input} from 'semantic-ui-react';
+
+class SearchBar extends Component {
+
+    pesquisaTermo = e => {
+        
+        if(e.keyCode === 13) {
+            const termo = e.target.value
+            console.log(termo)
+        }
+       
+    }
+    render(){
+        return (
+            <div className='search-bar'>
+                <Segment stacked>
+                    <Input icon='search' onKeyDown={(e) => this.pesquisaTermo(e)} size='large' placeholder='Seach ...' />
+                </Segment>
+            </div>
+        )
+    }
+    
+}
+
+export default SearchBar
---
+++ src/service/api.js
@@ -0,0 +1,3 @@
+const API_KEY = 'AIzaSyB7k4gplBfuyL5OzqxX7lFWR3GzALxey-c'
+
+export default API_KEY
