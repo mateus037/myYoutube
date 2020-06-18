eb125dbb7039639cd9abc19bfbd1c7ebebe38b1e
mateus rodrigues <mateus037@gmail.com>
2020-06-18 12:33:20 -0300

    Conectando o componente VideoList a Store

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,11 +1,47 @@
 import React from 'react'
+import { connect } from "react-redux";
+import { List, Image } from 'semantic-ui-react'
 
 const VideoList = props => {
     return (
         <div className='video-list'>
-            <h1>Video List</h1>
+            <List animated verticalAlign='middle'>
+                <List.Item>
+                    <Image avatar src='' />
+
+                    <List.Content>
+                        <List.Header>Titulo do Video</List.Header>
+                    </List.Content>
+                </List.Item>
+
+                <List.Item>
+                    <Image avatar src='' />
+
+                    <List.Content>
+                        <List.Header>Titulo do Video</List.Header>
+                    </List.Content>
+                </List.Item>
+
+                <List.Item>
+                    <Image avatar src='' />
+
+                    <List.Content>
+                        <List.Header>Titulo do Video</List.Header>
+                    </List.Content>
+                </List.Item>
+
+            </List>
+    <p>{JSON.stringify(props)}</p>
         </div>
     )
 }
 
-export default VideoList
+const mapStateToProps = (state) => {
+    return {
+        videos: state.busca.videos,
+        carregando: state.busca.carregando,
+        erro: state.busca.erro
+    }
+}
+
+export default connect(mapStateToProps, null)(VideoList)
